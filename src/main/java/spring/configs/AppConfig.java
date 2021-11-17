bb3489251523590d99689a2e9a3fba6f5cb3ae7d
nvdinh09thxd <nvdinh09thxd@gmail.com>
2021-11-17 16:13:15 +0700

    initial commit

---
+++ src/main/java/spring/configs/AppConfig.java
@@ -0,0 +1,20 @@
+package spring.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScans({ @ComponentScan(value = "spring.*") })
+public class AppConfig {
+	@Bean("viewResolver")
+	public InternalResourceViewResolver getViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+}
---
+++ src/main/java/spring/controllers/BasicController.java
@@ -0,0 +1,15 @@
+package spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("basic")
+public class BasicController {
+
+	@GetMapping("index")
+	public String list() {
+		return "basic/index";
+	}
+}
